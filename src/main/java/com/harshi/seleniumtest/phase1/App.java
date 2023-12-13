512c382ad8f9460b48ee104c8d867fbf5c9a4e88
harshithanap <ashwathharshitha0@gmail.com>
2023-12-13 14:09:23 +0530

    project setup for sikuli

---
+++ src/main/java/com/harshi/seleniumtest/phase1/App.java
@@ -0,0 +1,13 @@
+package com.harshi.seleniumtest.phase1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
